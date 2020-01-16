f2961df0a32c115daf5b1616125bbecb3b50b097
pierrexyz <pierrexyz@gmail.com>
2020-01-16 18:20:37 +0100

    full resum and accuracydone ; pybird in taylor pipeline done

--- make_taylorgrid.py
+++ make_taylorgrid.py
@@ -1,78 +1,66 @@
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
+# from mpi4py import MPI
+# comm = MPI.COMM_WORLD
+# size = comm.Get_size()
+# rank = comm.Get_rank()
+rank = 0
 
 import numpy as np
 import os
 import sys
 import Grid
-import pybird
+import pybird_fullresum as pybird
 import copy
 import time
 
-time.sleep(4)
+#time.sleep(4)
 
-basedir = '../homegroup/'
+basedir = './'
 OUTPATH = os.path.join(basedir, "output")
 outpk = os.path.join(basedir, "Pk")
 
 
-ncores = size
-nrun = int(sys.argv[1])
-runs = int(sys.argv[2])
+ncores = 1#size
+nrun = 1#int(sys.argv[1])
+runs = 1#int(sys.argv[2])
 lenrun = int(len(Grid.flattenedgrid) / runs)
 thetarun = Grid.flattenedgrid[nrun * lenrun:(nrun+1) * lenrun]
 Ntot = len(thetarun)
 sizered = int(Ntot/ncores)
 
-arrayred = thetarun[rank * sizered:(rank+1) * sizered]
+arrayred = Grid.flattenedgrid #thetarun[rank * sizered:(rank+1) * sizered]
+sizearray = len(arrayred)
 
 freepar = Grid.freepar
-print("lenrun, sizered", lenrun, sizered)
+#print("lenrun, sizered", lenrun, sizered)
+
 ### To create outside the grid
 nonlinear = pybird.NonLinear(load=True,save=True)
 resum = pybird.Resum()
+kbird = pybird.common.k
+allk = np.concatenate([kbird, kbird]).reshape(-1,1)
+
 allPlin = []
 allPloop = []
 for i, theta in enumerate(arrayred):
     parameters = copy.deepcopy(Grid.parref)
     truetheta = Grid.valueref + theta * Grid.delta
-    idx = nrun * lenrun + rank * sizered + i
-    print("nrun, rank, i", nrun, rank, i)
+    #idx = nrun * lenrun + rank * sizered + i
+    #print("nrun, rank, i", nrun, rank, i)
+    idx = i
+    print ("i on tot", i, sizearray)
+
     parameters["PathToOutput"] = os.path.join(OUTPATH, 'output' + str(nrun) + str(rank) + str(i))
     for k, var in enumerate(freepar):
         parameters[var] = truetheta[k]
     # parameters['h'] = 1/parameters['invh']
     kin, Plin, z, Omega_m = Grid.CompPterms(parameters)
     bird = pybird.Bird(kin, Plin, Omega_m, z, full=False)
-    nonlinear.PsCf(bird, window=None)
+    nonlinear.PsCf(bird)
     bird.setPsCfl()
     resum.Ps(bird, full=False)
     bird.subtractShotNoise()
-    fvec = [1, 2 * bird.f, bird.f**2]
-    fvec2 = [ 2, 2, 2, 2 * bird.f, 2 * bird.f, 2 * bird.f]
-    ### the 1-loop resummed power spectrum is stored in:
-    # bird.P11l = np.empty(shape=(co.Nl, co.N11, co.Nk))
-    # bird.Ploopl = np.empty(shape=(co.Nl, co.Nloop, co.Nk))
-    # bird.Pctl = np.empty(shape=(co.Nl, co.Nct, co.Nk))
-    ### where:
-    # Nl = 2 : number of multipoles
-    # N11 = 3, Nloop = 12, Nct = 6
-    # Nk : number of k points
-    ### the k points are:
-    k = pybird.common.k
-    # Note that in Plinear we have an opposite convention
-    Plin = np.einsum('b,lbx->lbx', fvec, bird.P11l)
-    Plin = np.flip(Plin, 1)
-    Plin = np.concatenate((np.tile(k.reshape((50,1,1)), (1,3,2)), np.swapaxes(Plin, 0, 2)), axis=1)
-    Plin = np.vstack((Plin[..., 0], Plin[..., 1]))
-    Ploop1 = np.swapaxes(bird.Ploopl, 0, 2)
-    Ploop2 = np.einsum('b,lbx->lbx', fvec2, bird.Pctl)
-    Ploop2 = np.swapaxes(Ploop2, 0, 2)
-    Ploopl0 = np.hstack((k.reshape((len(k), 1)), Ploop1[..., 0], Ploop2[..., 0]))
-    Ploopl1 = np.hstack((k.reshape((len(k), 1)), Ploop1[..., 1], Ploop2[..., 1]))
-    Ploop = np.vstack((Ploopl0, Ploopl1))
+    
+    Plin, Ploop = bird.formatTaylor()
     idxcol = np.full([Plin.shape[0], 1], idx)
     allPlin.append(np.hstack([Plin, idxcol]))
     allPloop.append(np.hstack([Ploop, idxcol]))
